d9370eb913997a30cb8002d283deebcedfc48f62
alexandrage <alexandrage@mail.ru>
2020-11-13 17:19:31 +0300

    release up

--- src/board/ScoreboardRun.java
+++ src/board/ScoreboardRun.java
@@ -1,10 +1,14 @@
 package board;
 
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class ScoreboardRun extends BukkitRunnable {
@@ -30,27 +34,7 @@ public class ScoreboardRun extends BukkitRunnable {
 			if (board == null) {
 				continue;
 			}
-
-			for (Player p : Bukkit.getOnlinePlayers()) {
-				String pref = trim(this.plugin.chat.getPlayerPrefix(p));
-				String suff = trim(this.plugin.chat.getPlayerSuffix(p));
-				Team team = board.getScoreboard().getTeam(p.getName());
-				if (team == null) {
-					team = board.getScoreboard().registerNewTeam(p.getName());
-				}
-				team.setPrefix(pref);
-				team.setSuffix(suff);
-				team.addEntry(p.getName());
-			}
-
-			for (Team t : board.getScoreboard().getTeams()) {
-				String name = t.getName();
-				Player p = Bukkit.getPlayerExact(name);
-				if (p == null && name.length() > 2) {
-					t.unregister();
-				}
-			}
-
+			this.setupTeams(board.getScoreboard());
 			List<String> scoreList = this.plugin.getConfig().getStringList("scoreList");
 			Collections.reverse(scoreList);
 			for (int i = 0; i < scoreList.size(); i++) {
@@ -68,6 +52,41 @@ public class ScoreboardRun extends BukkitRunnable {
 		}
 	}
 
+	public void setupTeams(Scoreboard board) {
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			String pref = trim(this.plugin.chat.getPlayerPrefix(p));
+			String suff = trim(this.plugin.chat.getPlayerSuffix(p));
+			Team team = board.getTeam(p.getName());
+			if (team == null) {
+				team = board.registerNewTeam(p.getName());
+			}
+			team.setColor(ChatColor.getByChar(this.getColor(pref)));
+			team.setPrefix(pref);
+			team.setSuffix(suff);
+			team.addEntry(p.getName());
+		}
+		for (Team t : board.getTeams()) {
+			String name = t.getName();
+			Player p = Bukkit.getPlayerExact(name);
+			if (p == null && name.length() > 2) {
+				t.unregister();
+			}
+		}
+	}
+
+	public char getColor(String pref) {
+		char color = 'r';
+		char[] c = pref.toCharArray();
+		for (int i = 0; i < pref.length(); i++) {
+			if (c[i] == '' && i < pref.length()) {
+				if (patern.matcher(String.valueOf(new char[] { c[i], c[i + 1] })).matches()) {
+					color = c[i + 1];
+				}
+			}
+		}
+		return color;
+	}
+
 	private String trim(String name) {
 		String color = translateOld('&', name);
 		if (color.length() > 64)
@@ -79,10 +98,15 @@ public class ScoreboardRun extends BukkitRunnable {
 		final char[] b = textToTranslate.toCharArray();
 		for (int i = 0; i < b.length - 1; ++i) {
 			if (b[i] == altColorChar && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i + 1]) > -1) {
-				b[i] = '§';
+				b[i] = '';
 				b[i + 1] = Character.toLowerCase(b[i + 1]);
 			}
 		}
 		return new String(b);
+	}
+
+	private static Pattern patern;
+	static {
+		patern = Pattern.compile("(?i)" + String.valueOf('') + "[0-9A-FK-OR]");
 	}
 }
